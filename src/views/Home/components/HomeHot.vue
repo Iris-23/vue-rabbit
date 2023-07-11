b1c54ffbf3745019e6222637c5f7e533ee35a3e5
Iris <2545237915@qq.com>
2023-07-11 15:39:08 +0800

    图片懒加载指令实现

--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -11,13 +11,14 @@ getHotList()
 
 </script>
 
+<!-- 图片懒加载 -->
 <template>
   <HomePanel title="人气推荐" sub-title="人气爆款 不容错过">
     <template #main>
         <ul class="goods-list">
         <li v-for="item in hotList" :key="item.id">
           <RouterLink to="/">
-            <img :src="item.picture" alt="">
+            <img v-img-lazy="item.picture"  alt="">
             <p class="name">{{ item.title }}</p>
             <p class="desc">{{ item.alt }}</p>
           </RouterLink>
